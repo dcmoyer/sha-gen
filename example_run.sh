081070dc8b6cfbc5fd518ab1ab06cbb82b0d9bed
Haley M Sanchez <haleysan@peppercorn.csail.mit.edu>
2022-04-21 12:08:10 -0400

    updating example_run.sh

--- example_run.sh
+++ example_run.sh
@@ -3,11 +3,12 @@
 #SOURCE THE APPROPRIATE PYTHON PATH HERE
 #source /data/vision/polina/scratch/dmoyer/bash_source.sh
 
-PYTHONPATH="src/:${PYTHONPATH}" python \
-  scripts/runner.py \
-  --config configs/test-v3.toml
+# PYTHONPATH="src/:/data/vision/polina/scratch/fetal-reorg/src/:${PYTHONPATH}" python \
+#   src/runner_2.py \
+#   --config configs/test-v3.toml
 
 
-
+PYTHONPATH="src/:/data/vision/polina/scratch/fetal-reorg/src/:${PYTHONPATH}" python \
+  src/eval.py 
 
 
